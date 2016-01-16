244ac5bd826a47ae128d9599abf6baabddf0d1a2
vidarlx <lax@fea.st>
2016-01-16 11:52:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var Server = require('./lib/network/Server');
+var Game = require('./lib/game/Game.js');
+
+(function () {
+    var s = new Server();
+    var g = new Game();
+    g.setServer(s);
+})();
