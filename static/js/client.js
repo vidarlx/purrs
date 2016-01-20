528135892078c7114bf05ab17cf8d9ee8c7b5cbd
vidarlx <lax@fea.st>
2016-01-21 00:36:21 +0100

    drawing mode available (basic version)

--- static/js/client.js
+++ static/js/client.js
@@ -1,12 +1,68 @@
-function showPlayers(players) {
-    var list = [];
-    var playersIDs = Object.keys(players);
-    
-    var player = null;
-    for (var i = 0; i < playersIDs.length; i++) {
-        player = players[playersIDs[i]];
-        list.push('<li>' + player.name + '</li>');
+var isDrawingPlayer = false;
+var currentImage = null;
+var timer = null;
+
+var GameClient = function () {
+    function showPlayers(players) {
+        var list = document.querySelector('#playersList > ul');
+        // clear all childs before redraw
+        list.innerHTML = '';
+
+        var playersIDs = Object.keys(players);
+
+
+        if (playersIDs.length == 1) {
+            console.log('First player: drawing mode enabled')
+            isDrawingPlayer = true;
+
+            timer = setTimeout(function () {
+                sendCurrentImage();
+            }, 2000);
+
+        }
+
+        var player = null;
+        for (var i = 0; i < playersIDs.length; i++) {
+            player = players[playersIDs[i]];
+
+            var li = document.createElement('li');
+            li.innerHTML = player.name;
+            li.addEventListener('click', function () {
+                newGame(player.name);
+            }, false);
+
+            list.appendChild(li);
+        }
     }
-    
-    document.querySelector('#playersList > ul').innerHTML = list;
-}
+
+    var newGame = function (player) {
+        NetworkClient.newGame(player);
+    }
+
+    var sendCurrentImage = function () {
+        if (currentImage) {
+            console.log('Client | Sending image to the server');
+            NetworkClient.sendImage(JSON.stringify(currentImage));
+        }
+        clearTimeout(timer);
+
+        timer = setTimeout(function () {
+            sendCurrentImage();
+        }, 500);
+    }
+
+    var redrawImage = function (image) {
+        if (currentImage) {
+            console.log('Client | Redrawing image');
+            currentImage.clear();
+            currentImage.loadFromJSON(image, currentImage.renderAll.bind(currentImage));
+        }
+    }
+
+    return {
+        showPlayers: showPlayers,
+        redrawImage: redrawImage
+    }
+}();
+
+
--- static/js/network.js
+++ static/js/network.js
@@ -1,5 +1,35 @@
-(function () {
-    socket.on('playersListUpdated', function (players) {
-        showPlayers(players);
-    });
-})();
+var NetworkClient = function () {
+    function handleEvents() {
+        socket.on('playersListUpdated', function (players) {
+            GameClient.showPlayers(players);
+        });
+
+        socket.on('image_updated', function (image) {
+            GameClient.redrawImage(image);
+        });
+
+    }
+
+    function newGame(player) {
+        var players = {
+            p1: socket.id,
+            p2: player
+        };
+
+        console.log('NetworkClient | Emit new_game event');
+        socket.emit('new_game', players);
+    }
+
+    function sendImage(image) {
+        console.log('NetworkClient | Emit send_image event');
+        socket.emit('send_image', image);
+    }
+
+    return {
+        handleEvents: handleEvents,
+        newGame: newGame,
+        sendImage: sendImage
+    };
+}();
+
+NetworkClient.handleEvents();
